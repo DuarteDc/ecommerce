e78deb513e360c6536b95878fd9ea0d31a78abdf
Eduardo Duarte <al221910409@gmail.com>
2022-03-03 12:39:01 -0500

    cambios

--- src/components/Layouts/Content.jsx
+++ src/components/Layouts/Content.jsx
@@ -27,7 +27,7 @@ const Content = ({ children, offers }) => {
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>Love Nails</title>
             <link rel="preconnect" href="https://fonts.googleapis.com"/>
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" CrossOrigin/>
             <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700&family=Poppins:wght@200;300;700&display=swap" rel="stylesheet"/>
            </Head>
             <TopHeader/>
