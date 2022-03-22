b9e7c457880ceda750afd29d676d81e86c1ba40b
RodrigoRA03 <al221511338gmail.com>
2022-03-22 11:41:20 -0600

    Soluciiones error eslint core web vitals

--- src/components/Layouts/NavBar.jsx
+++ src/components/Layouts/NavBar.jsx
@@ -141,7 +141,7 @@ const NavBar = () => {
       <div className="px-2 pt-2 pb-3 space-y-1">
       {
         routes.map(route=>(
-          <Link href={route.path}>
+          <Link href={route.path} key={route.path}>
             <span href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">{route.name}</span>
           </Link>
         ))
